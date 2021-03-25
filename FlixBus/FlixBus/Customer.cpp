331447a8fb231a9af3121cc5988f2b96be7295b5
shrikepoint <madtip00@gmail.com>
2021-03-25 13:47:20 -0500

    method prototypes

--- FlixBus/FlixBus/Customer.cpp
+++ FlixBus/FlixBus/Customer.cpp
@@ -1,2 +1,60 @@
 //implementation file
 #include "Customer.h"
+
+void Customer::setFirstName(string fName)
+{
+}
+
+void Customer::setLastName(string lName)
+{
+}
+
+void Customer::setAddress(string address)
+{
+}
+
+void Customer::setEmail(string email)
+{
+}
+
+void Customer::setNumber(int number)
+{
+}
+
+void Customer::addTicket(Ticket ticket)
+{
+}
+
+void Customer::removeTicket(int ticketId)
+{
+}
+
+string Customer::getFirstName()
+{
+	return string();
+}
+
+string Customer::getLastName()
+{
+	return string();
+}
+
+string Customer::getAddress()
+{
+	return string();
+}
+
+string Customer::getEmail()
+{
+	return string();
+}
+
+int Customer::getNumber()
+{
+	return 0;
+}
+
+vector<Ticket> Customer::getTickets()
+{
+	return vector<Ticket>();
+}
--- FlixBus/FlixBus/Customer.h
+++ FlixBus/FlixBus/Customer.h
@@ -6,9 +6,24 @@ using namespace std;
 
 
 class Customer {
-public:
-	string firstName, lastName, address, email;
+private:
 	int userID;
+	string firstName, lastName, address, email;
+	int contactNumber;
 	vector<Ticket> tickets;
-	vector<string> datesOfTravel;
+public:
+	void setFirstName(string fName);
+	void setLastName(string lName);
+	void setAddress(string address);
+	void setEmail(string email);
+	void setNumber(int number);
+	void addTicket(Ticket ticket);
+	void removeTicket(int ticketId);
+	string getFirstName();
+	string getLastName();
+	string getAddress();
+	string getEmail();
+	int getNumber();
+	vector<Ticket> getTickets();
+
 };
