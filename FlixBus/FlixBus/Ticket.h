debe54794979a8e00eefcb66b6091542e93a5d51
shrikepoint <madtip00@gmail.com>
2021-03-25 13:09:54 -0500

    ticket h

--- FlixBus/FlixBus/Ticket.h
+++ FlixBus/FlixBus/Ticket.h
@@ -6,6 +6,8 @@ using namespace std;
 
 class Ticket{
 public:
+	//should have attribute for date and time booked to potentially compare to time and date of route departure versus cancellation date and time
+	bool active; //active or cancelled ticket
 	Route route;
 	double price;
 };
