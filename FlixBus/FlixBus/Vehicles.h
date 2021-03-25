a43aa34b5ec13126679ccd85a396f96bc6ee4b8e
Mason Tipton <53275118+shrikepoint@users.noreply.github.com>
2021-03-25 12:59:06 -0500

    Update Vehicles.h

--- FlixBus/FlixBus/Vehicles.h
+++ FlixBus/FlixBus/Vehicles.h
@@ -5,8 +5,17 @@ using namespace std;
 
 class Vehicles{
 public:
+	int idNo; //unique identifier for bus
 	string type;
+	int capacity; //how many seats-- this is implicitly linked to type.
+	int ratePerMile; /* again, implicitly linked to type. While it's in decimal numbers, it's best to put it in integers (i.e. 0.16 per mile
+	being stored 16 pennies per mile so we can avoid double and float shenanigans */
 	int count;
 	int seats; // FIXME: maybe we can use arrays?
-};
+	//I don't think that will be necessary, I think we can potentially make a Seat class, assigning bus number, row and column, and boolean
+	//saying if its currently booked or not
+	//i think we CAN make an array of that object though
 
+
+	//potentially want to have an attribute of an array of bus drivers who drive said bus
+};
